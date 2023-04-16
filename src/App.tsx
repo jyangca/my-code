17951c1ea3ee74194e586c6321db61a868f89d90
Jiho YANG <yjsm111@nuua.ai>
2023-04-16 21:25:10 +0900

    ADD: Add styled Container and Tree components to App.tsx and import index.css in main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
-import { useState } from 'react'
+import { Container } from './styles'
+import { Tree } from 'components'
 
-function App() {
-    const [count, setCount] = useState(0)
-
-    return <div className="App">{count}</div>
+export default function App() {
+    return (
+        <Container>
+            <Tree name="main" defaultOpen>
+                <Tree name="hello" />
+                <Tree name="subtree with children">
+                    <Tree name="hello" />
+                    <Tree name="sub-subtree with children">
+                        <Tree name="child 1" style={{ color: '#37ceff' }} />
+                        <Tree name="child 2" style={{ color: '#37ceff' }} />
+                        <Tree name="child 3" style={{ color: '#37ceff' }} />
+                        <Tree name="custom content">
+                            <div
+                                style={{
+                                    position: 'relative',
+                                    width: '100%',
+                                    height: 200,
+                                    padding: 10,
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        width: '100%',
+                                        height: '100%',
+                                        background: 'black',
+                                        borderRadius: 5,
+                                    }}
+                                />
+                            </div>
+                        </Tree>
+                    </Tree>
+                    <Tree name="hello" />
+                </Tree>
+                <Tree name="world" />
+                <Tree name={<span>🙀 something something</span>} />
+            </Tree>
+        </Container>
+    )
 }
-
-export default App
